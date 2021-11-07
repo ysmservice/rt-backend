2ff2f05b2ae0d250af27bac34a3a13e9d5aa2e15
tasuren <tasuren_0618@yahoo.co.jp>
2021-11-07 13:54:24 +0900

    new: ping用のWebSocketを作った。

--- blueprints/normal.py
+++ blueprints/normal.py
@@ -8,16 +8,8 @@ from backend import TypedBlueprint, WebSocket, PacketData, logger
 bp = TypedBlueprint("Normal")
 
 
-@bp.route("/ping")
-async def ping(_):
-    return response.text("pong")
-
-
-@bp.websocket("/wstest")
+@bp.websocket("/ping")
 class WSPing(WebSocket):
     async def ping(self, data: PacketData) -> str:
         logger.debug(f"Received ping: {data}")
         return "pong"
-
-    async def print(self, data: PacketData) -> None:
-        logger.info(f"WebSocket Test Print: {data}")
