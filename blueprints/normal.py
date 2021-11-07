7eda5597cf24e6b802b8e7e7db997baa95470f68
tasuren <tasuren_0618@yahoo.co.jp>
2021-11-07 14:21:57 +0900

    new: faviconのリダイレクトを追加した。

--- blueprints/normal.py
+++ blueprints/normal.py
@@ -13,3 +13,8 @@ class WSPing(WebSocket):
     async def ping(self, data: PacketData) -> str:
         logger.debug(f"Received ping: {data}")
         return "pong"
+
+
+@bp.route("/favicon.ico")
+async def favicon_redirect(_):
+    return response.redirect("/img/favicon.ico")
