a043689f8c5ffb788e710c25ff7707a7645e9a8a
tasuren <45121209+tasuren@users.noreply.github.com>
2022-01-24 14:10:14 +0900

    update: TTSを除外した。

--- blueprints/api/__init__.py
+++ blueprints/api/__init__.py
@@ -11,7 +11,6 @@ from .captcha import bp as captcha_bp, on_load as captcha_on_load
 from .guild import bp as guild_bp, on_load as guild_on_load
 from .oauth import bp as oauthbp, on_load as oauth_on_load
 from .news import bp as newsbp, on_load as news_on_load
-from .tts import bp as ttsbp, on_load as tts_on_load
 from .short_url import bp as short_url_bp
 from .reprypt_api import bp as repryptbp
 from .normal import bp as testbp
@@ -19,11 +18,11 @@ from .help import bp as helpbp
 
 
 blueprints = (
-    testbp, helpbp, short_url_bp, newsbp, ttsbp, oauthbp, captcha_bp, repryptbp,
+    testbp, helpbp, short_url_bp, newsbp, oauthbp, captcha_bp, repryptbp,
     guild_bp
 )
 on_loads = (
-    news_on_load, tts_on_load, oauth_on_load, captcha_on_load, guild_on_load
+    news_on_load, auth_on_load, captcha_on_load, guild_on_load
 )
 bp = TypedBlueprint.group(*blueprints, url_prefix="/api")
 
