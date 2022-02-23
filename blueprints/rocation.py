8950343b0317f48e69cd9a2841beeb66d238cebf
tasuren <tasuren_0618@yahoo.co.jp>
2022-02-23 21:42:31 +0900

    update: 作りかけ。

--- blueprints/rocation.py
+++ blueprints/rocation.py
@@ -1,12 +1,11 @@
 # RT - Rocation
 
-from sanic.response import HTTPResponse, redirect, html
 from sanic.request import Request
 from sanic import Blueprint, __app__
 
-from backend.utils import CoolDown, api
+from ujson import loads, dumps
 
-from data import TEMPLATE_FOLDER
+from backend.utils import CoolDown, api
 
 
 bp = Blueprint("Rocations", "/rocations")
@@ -14,9 +13,10 @@ TABLE = "Rocations"
 
 
 async def row2dict(row: tuple) -> dict:
+    data = await __app__.ctx.rtc.request("get_guild", row[0])
     return {
-        "name": (await __app__.ctx.rtc.request("get_guild", row[0]))["name"],
-        "description": row[1], "tags": row[2], "nices": row[3], "invite": row[4],
+        "name": data["name"], "icon": data["avatar_url"],
+        "description": row[1], "tags": loads(row[2]), "nices": loads(row[3]), "invite": row[4],
         "raised": row[5]
     }
 
@@ -24,12 +24,12 @@ async def row2dict(row: tuple) -> dict:
 @bp.route("/api/gets/<page:int>")
 @CoolDown(7, 10)
 async def page(request: Request, page: int):
-    assert 0 <= page < 50, "これ以上ページを開けません。"
-    page = 10 * page - 10
+    assert 1 <= page < 50, "これ以上ページを開けません。"
+    page = 9 * page - 9
     async with request.app.ctx.pool.acquire() as conn:
         async with conn.cursor() as cursor:
             await cursor.execute(
-                f"SELECT * FROM {TABLE} WHERE language = %s ORDER BY raised LIMIT {page}, 10;",
+                f"SELECT * FROM {TABLE} WHERE language = %s ORDER BY raised LIMIT {page}, 9;",
                 (request.args.get("language", "ja"),)
             )
             rows = await cursor.fetchall()
@@ -37,6 +37,34 @@ async def page(request: Request, page: int):
         datas = {}
         for row in filter(lambda row: bool(row), rows):
             datas[row[0]] = await row2dict(row)
+        for i in range(8):
+            datas[i]  ={
+            "name": "RTサーバー",
+            "description": """VALORANTでランクやカスタムに一緒に行くフレンド欲しくないですか？
+そんな思いでサーバーを作成しました！
+😆活発なサーバーになるように盛り上げていきましょう！
+
+【募集対象】
+・🔰初心者から上級者まで（オーナーはゴールド帯です）
+・今上手くなくても、上手くなっていきたい方
+・♂♀性別・年齢関係なし
+
+【特徴】
+・bot導入により快適な利用ができます。😁 さらに快適に使用できるよう要望もお待ちしております。
+・ランクごとにチャンネルが分かれているので、同じランクの方と交流しやすいです（人が増えればw）🤝
+・雑談チャンネルもあるので、プレイに疲れたらおしゃべりしましょう！
+・女性メンバーも在籍し、活発に活動しています！
+
+【参加NG（No participation）】
+・ルールを守れない方
+・人が嫌がることをする方
+・日本語が話せない方（Those who do not speak Japanese.）
+・ランクでスマーフ（自分の最高ランクより下のランクでプレイ）する方、またそれを推奨している方""",
+            "tags": [chr(x) for x in range(78+3*i-3, 78+3*i)],
+            "nices": [],
+            "invite": "https://discord.gg/vPygd4UJnf",
+            "raised": 1645520000
+        }
         return api("Ok", datas)
     else:
         return api("Error", None, 404)
